a326f975367654d8a9660c9c8ec99af820ab8a7d
Bajrang Panigrahi <{40162966}+{bajrang0789}@users.noreply.github.com>
2024-12-14 08:19:13 +0000

    be 1st

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 # git checkout -b bpanigrah/build
 git pull origin bpanigrah/build
 gcloud config set project bajrang-444504
-pushd sem-fornt-end
+pushd sem-back-end
 bash script.sh
 popd
-pushd sem-back-end
+pushd sem-fornt-end
 bash script.sh
 popd
 
